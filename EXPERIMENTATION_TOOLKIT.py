63f130cb17ae7da5d1dc6be9dda5b24085b4ce37
Stanisław Wasilewski <wasilewski.sf@gmail.com>
2024-06-21 01:44:43 +0200

    finally

--- EXPERIMENTATION_TOOLKIT.py
+++ EXPERIMENTATION_TOOLKIT.py
@@ -3,8 +3,11 @@ import EVERYTHING_experiment as experiment
 import random
 import time
 import random
+import numpy as np
+import pandas as pd
+import ast
 
-def run_experiments(duration, num_rows, filename, num_rabbits, num_foxes, rabbit_range = (2, 100), fox_range = (2, 100)):
+def run_experiments(duration, num_rows, filename, rabbit_range = (2, 100), fox_range = (2, 100)):
         # Generate each row using the generate() function
         for i in range(num_rows):
             num_rabbits = random.randint(rabbit_range[0], rabbit_range[1])
@@ -29,8 +32,6 @@ def run_experiments(duration, num_rows, filename, num_rabbits, num_foxes, rabbit
 # Example use:
 #run_experiments(200, 500, 'final_data.csv', 20, 3)
 
-
-
 # Random data generator for testing the neural network
 def generate_random_data(num_entries, num_rows, filename):
     # Open the file for writing
@@ -49,3 +50,33 @@ def generate_random_data(num_entries, num_rows, filename):
 
 # Example usage:
 # generate_random_data(20, 30, 'fake_data.csv')
+
+
+
+def load_and_parse_data(file_path):
+    data = pd.read_csv(file_path, header=None)
+    sequences = []
+    for index, row in data.iterrows():
+        # Convert the row from string representation to list of lists of integers
+        sequence = ast.literal_eval(row[0])
+        sequences.append(sequence)
+    return np.array(sequences)
+
+
+
+
+def split_sequences(sequences, length):
+    discard_counter = 0
+
+    inputs = []
+    targets = []
+    for seq in sequences:
+        if len(seq) >= length:
+            mid = len(seq) // 2
+            inputs.append(seq[:mid])
+            targets.append(seq[mid:])
+        else:
+            discard_counter += 1
+
+    print(f"Discarded {discard_counter} sequences")
+    return np.array(inputs), np.array(targets)
